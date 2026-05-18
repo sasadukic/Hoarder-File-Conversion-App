2e87011983e331aa3d7d555bfbfe83536d429476
sasadukic <117151977+sasadukic@users.noreply.github.com>
2026-05-18 08:24:41 +0200

    fix: use pythonw to launch app without a terminal window

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 @echo off
-py "%~dp0main.py"
+for /f "delims=" %%i in ('py -c "import sys,os;print(os.path.join(os.path.dirname(sys.executable),'pythonw.exe'))"') do set PYTHONW=%%i
+start "" "%PYTHONW%" "%~dp0main.py"
